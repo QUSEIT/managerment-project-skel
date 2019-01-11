e8de7275c0189485a64ad539caf1e3e27d05b865
GGKevinGG <354785278@qq.com>
2019-01-11 18:07:14 +0800

    添加帖子js文件

---
+++ application/pages/management/Post/addPost.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class AddPost extends React.Component {
+    constructor (Props) {
+        super (Props)
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default AddPost
